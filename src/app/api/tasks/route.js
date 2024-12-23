279e757af0bc8539653db1c7cbfadfb02089896c
= <=>
2024-12-23 17:50:32 -0500

    v0.0.1

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,23 @@
 import { NextResponse } from "next/server";
+import { prisma } from '@/libs/prisma'
 
-export function GET() {
+export async function GET() {
+
+    const tasks = await prisma.task.findMany()
+    console.log(tasks);
+
+    return NextResponse.json(tasks);
+}
+
+export function POST() {
     return NextResponse.json("Creando tareas")
+}
+
+
+export function DELETE() {
+    return NextResponse.json("Eliminando")
+}
+
+export function PUT() {
+    return NextResponse.json("Actualizando tarea")
 }
